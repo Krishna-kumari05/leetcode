01eeefc49ee89fb6ce084dc09eb728da307db7e9
Krishna-kumari05 <krishna.kumari.csrl@gmail.com>
2026-07-29 15:00:13 +0530

    Time: 0 ms (100%), Space: 22.2 MB (82.95%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int i=1;
+        int n=ratings.size();
+        int sum=1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum++;
+                i++;
+                continue;
+            }
+            int peak=1;
+            while(i<n  && (ratings[i]>ratings[i-1])){
+                peak++;
+                sum+=peak;
+                i++;
+             
+            }
+            int down=1;
+            while(i<n && (ratings[i]<ratings[i-1]) ){
+                sum+=down;
+                i++;
+                down++;
+            }
+            if(peak<down){
+                sum=sum+(down-peak);
+            }
+        }
+        return sum;
+    }
+};
