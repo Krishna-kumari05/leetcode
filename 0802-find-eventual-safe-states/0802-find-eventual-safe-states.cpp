0ba89d5c3dda4727151c5c8f3b62292820a54c9f
Krishna-kumari05 <krishna.kumari.csrl@gmail.com>
2026-08-08 11:36:08 +0530

    Time: 4 ms (90.5%), Space: 52.6 MB (77.75%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int>indegree(n,0);
-        vector<int>ans;
-        vector<vector<int>>grid(n);
-        for(int i=0;i<n;i++){
-            for(auto it:graph[i]){
-                grid[it].push_back(i);
-                indegree[i]++;
-            }
-        }
-        queue<int>q;
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
-        while(!q.empty()){
-            int node=q.front();
-            ans.push_back(node);
-            q.pop();
-            for(auto it:grid[node]){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
+    bool dfs(vector<vector<int>>&graph,vector<int>&vis,vector<int>&pathvis,int node,vector<int>&ans){
+        vis[node]=1;
+        pathvis[node]=1;
+        for(auto it:graph[node]){
+            if(!vis[it]){
+                if(dfs(graph,vis,pathvis,it,ans)){     //m if there is a cycle it will be break down here 
+                    return true;
                 }
             }
-            
+            else if(pathvis[it]){
+                    return true;           // or here 
+                }
+        }
+
+        ans.push_back(node);
+        pathvis[node]=0;                // so we include nodes that are ssafe here ,here means dfs comppleted 
+        return false;                       // successfully with no cycles 
+
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int>ans;
+        vector<int>vis(n,0);
+        vector<int>pathvis(n,0);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(graph,vis,pathvis,i,ans);
+            }
         }
         sort(ans.begin(),ans.end());
         return ans;
