06862ac0c7662f2eaa6e9b0225c20c5381ed7ea2
Krishna-kumari05 <krishna.kumari.csrl@gmail.com>
2026-08-21 17:58:07 +0530

    Time: 0 ms (100%), Space: 10.1 MB (90.35%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev2=0;
+        int curi=0;
+        int prev=nums[0];
+        for(int i=1;i<nums.size();i++){
+            int take=nums[i]+prev2;
+            int nottake=prev;
+             curi=max(take,nottake);
+            prev2=prev;
+            prev=curi;
+        }
+        return prev;
+    }
+};
